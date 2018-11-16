4b573fa4931703a070e7b2be309fe715055d0590
peterzhen40 <yjzhen@qq.com>
2018-11-16 16:29:45 +0800

    V1.3.5-8:回退方法类，兼容旧版本

--- soap/src/main/java/com/zyy/soap/result/ResultUtil.java
+++ soap/src/main/java/com/zyy/soap/result/ResultUtil.java
@@ -1,5 +1,14 @@
 package com.zyy.soap.result;
 
+import com.zyy.soap.Soap;
+
+import org.ksoap2.SoapFault;
+import org.ksoap2.SoapFault12;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+
 /**
  * Error 帮助类
  *
@@ -33,4 +42,65 @@ public class ResultUtil {
         return false;
     }
 
+    public static String convertExceptionToString(Exception e) {
+        String suffix = "err-3333:";
+        if (e instanceof SocketTimeoutException) {
+            return suffix + "网络连接超时";
+        } else if (e instanceof XmlPullParserException) {
+            return suffix + "XML文件解析失败";
+        } else if (e instanceof SoapFault12 || e instanceof SoapFault) {
+            return suffix + "服务器出现异常";
+        } else if (e instanceof IOException) {
+            return suffix + "网络连接异常";
+        }
+
+        return suffix + e.getLocalizedMessage();
+    }
+
+    /**
+     * 没有数据
+     *
+     * @return
+     */
+    public static String convertNoData() {
+        String suffix = "err-3333:";
+        return suffix + "没有数据";
+    }
+
+    /**
+     * 解析返回错误信息
+     *
+     * @param data
+     * @return
+     */
+    public static String getError(String data, Soap.SYSTEM system) {
+        if (null == data || data.length() == 0) {
+            return "err-4444:返回数据为空";
+        }
+        String err = "";
+        if (data.startsWith("err-0000")) {
+            return data; //服务器错误返回所有的信息
+        } else {
+            try {
+                return data.split(":")[1];
+            } catch (Exception e) {
+                // 兼容以前的版本
+                if (!Soap.SYSTEM.JDYZB.equals(system)) {
+                    //民爆类项目
+                    if (ResultState.mErrorMap.containsKey(data)) {
+                        return ResultState.getErrorState(data);
+                    } else {
+                        return "解析错误,请联系管理员";
+                    }
+                } else {
+                    //剧毒类项目
+                    if (ResultStateForJDYzb.errMap.containsKey(data)) {
+                        return ResultStateForJDYzb.errMap.get(data);
+                    } else {
+                        return data;
+                    }
+                }
+            }
+        }
+    }
 }
